57913c2a82a0e8fe2d2050af05e86f9abc7a06ab
Alexander van Dam <alexiaan.vd@hotmail.com>
2019-03-13 18:59:33 +0100

    nieuwe aanpassingen toegevoegd

--- src/main/java/nl/schaapkabap/models/Product.java
+++ src/main/java/nl/schaapkabap/models/Product.java
@@ -34,6 +34,14 @@ public class Product {
     @JsonView(View.Public.class)
     private String description;
 
+    @Column(name = "price", nullable = false)
+    @JsonView(View.Public.class)
+    private double price;
+
+    @Column(name = "image", nullable = false)
+    @JsonView(View.Public.class)
+    private String image;
+
     @JoinColumn(name="fk_product_category")
     @ManyToOne(targetEntity = ProductCategory.class,
             cascade = CascadeType.PERSIST)
@@ -70,5 +78,21 @@ public class Product {
 
     public void setProductCategory(ProductCategory productCategory) {
         this.productCategory = productCategory;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 }
